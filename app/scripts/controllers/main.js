12107ab6f65db8b0e0cc8efe335798879937683e
tim geaney <timgeaney24@gmail.com>
2013-08-22 11:20:42 +0100

    added angular-ui

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,4 +3,13 @@
 angular.module('yeomanTestApp')
   .controller('MainCtrl', function ($scope) {
     $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+
+    $scope.addTodo = function(){
+    	$scope.todos.push($scope.todo);
+    	$scope.todo = '';
+    }
+
+    $scope.removeToDo = function(index){
+    	$scope.todos.splice(index, 1);
+    }
   });
