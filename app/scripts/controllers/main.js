96a51ca1a8fceb1e51e1cd07d0a87e8f7a368979
tim geaney <timgeaney24@gmail.com>
2013-08-23 14:36:32 +0100

    tidy up

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,13 +2,13 @@
 
 angular.module('yeomanTestApp')
   .controller('MainCtrl', function ($scope, localStorageService) {
-    //$scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
 
-    var todosInStore = localStorageService.get('todos');
-    $scope.todos =todosInStore && todosInStore.split('\n') || [];
-    $scope.$watch(function(){
-      localStorageService.add('todos', $scope.todos.join('\n'));
-    });
+    // var todosInStore = localStorageService.get('todos');
+    // $scope.todos =todosInStore && todosInStore.split('\n') || [];
+    // $scope.$watch(function(){
+    //   localStorageService.add('todos', $scope.todos.join('\n'));
+    // });
 
     $scope.addTodo = function(){
     	$scope.todos.push($scope.todo);
